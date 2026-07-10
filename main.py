e0fd6e55f8a5e3dd63ec050da607362bf5b08c74
basilreda <basilreda112211@gmail.com>
2026-07-10 12:01:59 +0300

    updated host in uvicorn

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "api.main:app",
-        host=os.getenv("HOST", "0.0.0.0"),
+        host=os.getenv("HOST", "127.0.0.1"),
         port=int(os.getenv("PORT", "8001")),
         reload=True,
     )
